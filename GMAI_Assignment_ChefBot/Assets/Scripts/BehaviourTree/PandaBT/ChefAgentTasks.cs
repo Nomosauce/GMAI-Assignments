523a8cc1cce3efd87add1bc8d0781b75dee189db
Nomosauce <sarah_atiqah05@hotmail.com>
2024-05-25 22:25:30 +0800

    scuffed go to counter AI

---
+++ GMAI_Assignment_ChefBot/Assets/Scripts/BehaviourTree/PandaBT/ChefAgentTasks.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Panda;
+using System.Runtime.CompilerServices;
+using UnityEngine.AI;
+
+public class ChefAgentTasks : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    [Task]
+    bool isNear(string tag1, string tag2)
+    {
+        Transform obj1 = GameObject.FindGameObjectWithTag(tag1).transform;
+        Transform obj2 = GameObject.FindGameObjectWithTag(tag2).transform;
+        float distance = 5f;
+
+        return Vector3.Distance(obj1.position, obj2.position) <= distance;
+    }
+
+    [Task]
+    void goTo(string tag)
+    {
+        Transform target = GameObject.FindGameObjectWithTag(tag).transform;
+        agent.SetDestination(target.position);
+        agent.stoppingDistance = 1f;
+
+        ThisTask.Succeed();
+    }
+
+    [Task]
+    bool idle()
+    {
+        return true;
+    }
+}
