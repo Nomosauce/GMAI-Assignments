5b1da1a6324393c927cd17244cf32d1f3870b2a8
Nomosauce <sarah_atiqah05@hotmail.com>
2024-06-01 23:05:08 +0800

    small refactoring at player stealrange

--- GMAI_Assignment_ChefBot/Assets/Scripts/BehaviourTree/PlayerController.cs
+++ GMAI_Assignment_ChefBot/Assets/Scripts/BehaviourTree/PlayerController.cs
@@ -67,12 +67,7 @@ public class PlayerController : MonoBehaviour
     bool withinStealingRange() //bool function to check if the player is near the table (where gold is kept)
     {
         Transform table = GameObject.FindGameObjectWithTag("Table").transform; //since we only need the position of the table for this function only its Transform is stored as a local var
-
-        if (Vector3.Distance(gameObject.transform.position, table.position) <= 5f) //vector.dist is used to find the dist between the first parameter and the second
-        {
-            return true; //returns true if distance between player and table is below or equal to 5
-        }
-        return false; //returns false if above 5
+        return Vector3.Distance(gameObject.transform.position, table.position) <= 5f; //vector.dist is used to find the dist between the first parameter and the second
     }
 
     void stealGold() //function that player calls when their input is detected
@@ -80,7 +75,5 @@ public class PlayerController : MonoBehaviour
         chef.chefsGold.SetActive(false); //disable the gold gameobject to make it seem like it was stolen
         stealText.SetActive(false); //disable text UI here because i did not want to use pandaBT task function for the player
         chef.isAngry = true;
-
-        //monster.SetActive(false); //personally wouldnt want the monster from the quest to stay put since its meant to only exist within the quest
     }
 }
